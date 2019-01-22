411f6a874151ddf1e63de0566f0e6af31df879a9
apshirokov <apshirokov@edu.hse.ru>
2019-01-22 19:51:22 +0300

    Delete position from map

--- app/src/main/java/apshirokov/cs/hse/iqueue/MapFragmentView.java
+++ app/src/main/java/apshirokov/cs/hse/iqueue/MapFragmentView.java
@@ -1,66 +1,30 @@
-/*
- * Copyright (c) 2011-2018 HERE Europe B.V.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package apshirokov.cs.hse.iqueue;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.here.android.mpa.common.Image;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.common.PositioningManager;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapMarker;
+import com.here.android.mpa.mapping.SupportMapFragment;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.here.android.mpa.common.GeoBoundingBox;
-import com.here.android.mpa.common.GeoCoordinate;
-import com.here.android.mpa.common.GeoPolygon;
-import com.here.android.mpa.common.GeoPolyline;
-import com.here.android.mpa.common.Image;
-import com.here.android.mpa.common.OnEngineInitListener;
-import com.here.android.mpa.mapping.Map;
-import com.here.android.mpa.mapping.MapCircle;
-import com.here.android.mpa.mapping.SupportMapFragment;
-import com.here.android.mpa.mapping.MapMarker;
-import com.here.android.mpa.mapping.MapPolygon;
-import com.here.android.mpa.mapping.MapPolyline;
 
-import android.support.v7.app.AppCompatActivity;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-/**
- * This class encapsulates the properties and functionality of the Map view.
- */
 public class MapFragmentView {
     private SupportMapFragment m_mapFragment;
     private Map m_map;
     private MapMarker m_map_marker;
     private AppCompatActivity m_activity;
-    private ImageView m_marker_button;
 
-    /**
-     * Initial UI button on map fragment view. It includes several buttons to add/remove map objects
-     * such as MapPolygon, MapPolyline, MapCircle and MapMarker.
-     *
-     */
+
     public MapFragmentView(SupportMapFragment m_mapFragment, AppCompatActivity activity) {
         this.m_mapFragment = m_mapFragment;
         m_activity = activity;
@@ -81,41 +45,31 @@ public class MapFragmentView {
             Log.e(this.getClass().toString(), "Failed to find intent name, NameNotFound: " + e.getMessage());
         }
 
-        boolean success = true;
-        if (!success) {
-            // Setting the isolated disk cache was not successful, please check if the path is valid and
-            // ensure that it does not match the default location
-            // (getExternalStorageDirectory()/.here-maps).
-            // Also, ensure the provided intent name does not match the default intent name.
-        } else {
-            if (m_mapFragment != null) {
-                /* Initialize the SupportMapFragment, results will be given via the called back. */
-                m_mapFragment.init(new OnEngineInitListener() {
-                    @Override
-                    public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
+        if (m_mapFragment != null) {
+            /* Initialize the SupportMapFragment, results will be given via the called back. */
+            m_mapFragment.init(new OnEngineInitListener() {
+                @Override
+                public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
 
-                        if (error == Error.NONE) {
-                            /*
-                             * If no error returned from map fragment initialization, the map will be
-                             * rendered on screen at this moment.Further actions on map can be provided
-                             * by calling Map APIs.
-                             */
-                            m_map = m_mapFragment.getMap();
+                    if (error == Error.NONE) {
+                        /*
+                         * If no error returned from map fragment initialization, the map will be
+                         * rendered on screen at this moment.Further actions on map can be provided
+                         * by calling Map APIs.
+                         */
+                        m_map = m_mapFragment.getMap();
 
-                            /*
-                             * Set the map center to the 4350 Still Creek Dr Burnaby BC (no animation).
-                             */
-                            m_map.setCenter(new GeoCoordinate(49.259149, -123.008555, 0.0),
-                                    Map.Animation.NONE);
+                        createMapMarker();
 
-                            /* Set the zoom level to the average between min and max zoom level. */
-                            m_map.setZoomLevel(14);
+                        //PositioningManager posManager = PositioningManager.getInstance();
+                        //posManager.start(PositioningManager.LocationMethod.GPS_NETWORK);
+                        //m_map.setCenter(posManager.getPosition().getCoordinate(), Map.Animation.NONE);
 
-                            createMapMarker();
-                        }
+                        // Display position indicator
+                        m_map.getPositionIndicator().setVisible(true);
                     }
-                });
-            }
+                }
+            });
         }
     }
 
